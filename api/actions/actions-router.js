4e1da34ecf096360d752827edb047e841ed71e86
jcpcabanada <jcpcabanada@gmail.com>
2022-01-14 14:53:34 -0800

    8th tests passing! need to do interview questions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -23,32 +23,61 @@ router.get('/', (req, res, next) => {
 //[GET] /api/actions/:id
 
 router.get('/:id', actionIdValidation, (req, res, next) => {
-
+    const { id } = req.params
+    Actions.get(id)
+        .then(action => {
+            res.json(action)
+        })
+        .catch(next)
 })
 
 
 //[POST] /api/actions
 
 router.post('/', (req, res, next) => {
-    // Actions.verb()
-    //     .then()
-    //     .catch()
+    const {project_id, description, notes} = req.body
+    if (!project_id || !notes || !description) {
+        res.status(400).json({
+            message: "Description and/or Notes are required fields!"
+        })
+    } else {
+        Actions.insert({project_id, description, notes})
+            .then(action => {
+                res.status(201).json(action)
+            })
+            .catch(next)
+    }
 })
 
 //[PUT] /api/actions/:id
 
 router.put('/:id', actionIdValidation, projectIdValidation, (req, res, next) => {
-    // Actions.verb()
-    //     .then()
-    //     .catch()
+    const { id } = req.params
+    const {project_id, description, notes, completed} = req.body
+    if (!project_id || !description || !notes || completed === undefined) {
+        res.status(400).json({
+            message: "Modified Action missing updated field!"
+        })
+    } else {
+        Actions.update(id, {project_id, description, notes, completed})
+            .then(modAction => {
+                res.json(modAction)
+            })
+            .catch(next)
+    }
 })
 
 //[DELETE] /api/actions/:id
 
 router.delete('/:id', actionIdValidation, (req, res, next) => {
-    // Actions.verb()
-    //     .then()
-    //     .catch()
+    const { id } = req.params
+    Actions.remove(id)
+        .then(delAction => {
+            res.status(404).json({
+                message: "Selected Action could not be found/deleted!"
+            })
+        })
+        .catch(next)
 })
 
 module.exports = router
